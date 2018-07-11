eb6b13d9c40be022c0b2285ff3bfa3578bf7447c
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-07-11 01:11:06 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,23 +1,31 @@
 require 'pry'
 
 class String
-
+  
   def sentence?
     if self.include?(".")
       return true
-      
-    else return false
+      else false
+  end
   end
 
   def question?
+    if self.include?("?")
+      return true
+    else return false
+  end
 
   end
 
   def exclamation?
+    if self.include?("!")
+      return true
+    else return false
+  end
 
   end
 
   def count_sentences
-
+self.split(/\.|\?|\!/).delete_if{|w| w.size < 2}.size
   end
 end
